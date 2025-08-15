5ce7c82ac5e6b0b4189b97505c16806b18574b55
Fabian Helm <fabian.helm@visma.com>
2025-08-15 07:47:14 +0200

    remove broken migration

--- supabase/migrations/20250813192652_0d9de8ec-aefd-42de-bb05-045a67031bbd.sql
+++
@@ -1,8 +0,0 @@
--- Add 'rooms' category to the item_category enum
-ALTER TYPE item_category ADD VALUE 'rooms';
-
--- Add a check constraint to ensure rooms can only be rented
-ALTER TABLE items ADD CONSTRAINT rooms_rental_only 
-CHECK (
-  category != 'rooms' OR listing_type IN ('rent', 'both')
-);
